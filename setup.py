b0381579ad07cb4779483f5b50084e968df95fea
begyy <begymrx@gmail.com>
2021-04-25 06:30:36 +0500

    🐛 pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="setuz",
+    version="1",
+    author="Sadullayev Bekhzod",
+    author_email="begymrx@gmail.com",
+    description="SetUz",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.5",
+    install_requires=['requests'],
+    url="https://github.com/set-uz/setuz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
